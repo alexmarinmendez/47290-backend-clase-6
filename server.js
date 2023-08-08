d7ac7e38308564d05294d9776ae030252ecde9b5
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-08-07 19:48:44 -0500

    feat: devolver string, json y html como response al cliente

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 //1. importamos la libreria express
 import express from 'express'
 
+const user = {
+    name: 'Alex',
+    last_name: 'Marin',
+    age: 46
+}
+
 //2. la ejecutamos (a la librería express)
 const app = express()
 
@@ -10,7 +16,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('/users', (req, res) => {
-    res.send('La lista de usuarios registrados en este sitio web es:')
+    res.send(user)
+})
+
+app.get('/home', (req, res) => {
+    res.send('<h1>Hola Mundo!!!</h1><hr /><h2>Bienvenido</h2><div style="color: blue">Soy un texto de color blue</div>')
 })
 
 //3. app es el servidor web, lo ponemos a escuchar peticiones en el puerto 8080
